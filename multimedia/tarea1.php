dfe372fe6bcda8a4a44214e377d78a9ce9620210
felipestay7 <felipeulc@live.cl>
2017-09-04 11:02:43 -0300

    respaldo xampp

---
+++ multimedia/tarea1.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Ingreso de datos</title>
+</head>
+<body>
+	<form action="ingreso.php" method="post">
+	<table>
+		<tr>
+			<td>Nombre</td>
+			<td><input type="text" name="txtNombre"></td>
+		</tr>
+		<tr>
+			<td>Apellido</td>
+			<td><input type="text" name="txtApellido"></td>
+		</tr>
+		<tr>
+			<td>Rut</td>
+			<td><input type="text" name="txtRut"></td>
+		</tr>
+		<tr>
+			<td>Promocion</td>
+			<td><input type="text" name="txtPromocion"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td><input type="submit" value="Ingresar Datos"></td>
+		</tr>
+	</table>
+	</form>
+</body>
